c588249d45affa70a8d1dfeb4c8db93009ddd559
harrybarcia12 <barcia.harry@gmail.com>
2022-08-12 13:22:21 -0700

    adding the user routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,6 +18,18 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('62f6a0b8e8151ffa6aa04410')
+        
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => {
+            console.log(err);
+            });
+});
+                
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const getDb=require('../util/database').getDb;
+const mongodb=require('mongodb');
+
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+save(){
+    const db=getDb();
+    return db.collection('users').insertOne(this);
+
+}
+
+static findById(userId){
+    const db=getDb();
+    return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
+
+}
+}
+module.exports=User;
